3c14d9a5b1bf7f2bf764a1f96c256ab6259349b0
Marko1212 <shihonage7@gmail.com>
2020-12-14 16:38:03 +0100

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,17 @@ import {Pizza} from './pizza';
 export class AppComponent {
   title = 'Pizza Party';
   name = '4 fromages';
-  maSuperPizza: Pizza = {
-    id: 1,
-    name: '4 fromages',
-    price: 12,
-    image: '4-fromages.jpg'
-  };
+  maSuperPizza: Pizza;
+
+  mesPizzas: Pizza[] = [
+    {id: 1, name: 'Reine', price: 12, image : "reine.jpg"},
+    {id: 2, name: '4 fromages', price: 13, image :"4-fromages.jpg"},
+    {id: 3, name: 'Orientale', price: 11, image : "orientale.jpg"},
+    {id: 4, name: 'Cannibale', price: 9, image : "cannibale.jpg"},
+  ];
+
+  onSelect(pizza: Pizza) {
+    console.log(pizza);
+    this.maSuperPizza = pizza;
+  }
 }
