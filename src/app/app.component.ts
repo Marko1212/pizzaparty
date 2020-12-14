2c5d7294523ca2fb30543328c8f547997183871a
Marko1212 <shihonage7@gmail.com>
2020-12-14 13:39:01 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Pizza} from './pizza';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pizzaparty';
+  title = 'Pizza Party';
+  name = '4 fromages';
+  maSuperPizza: Pizza = {
+    id: 1,
+    name: '4 fromages',
+    price: 12
+  };
 }
