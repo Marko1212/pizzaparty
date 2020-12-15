4d665d2d71734302a2e7e57b52cd2f5cdea55df3
Marko1212 <shihonage7@gmail.com>
2020-12-15 14:18:44 +0100

    update

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   isCollapsed = false;
-  @Input() navTitle;
+  @Input() navTitle : string;
+
+  //valeur par défaut : light
   @Input() color = "light";
 
   constructor() { }
